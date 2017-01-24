e28689e8a18cc5c7652e32134398fb977d7a8d27
Topher Webb <mining@ozecash.com>
2017-01-24 17:05:09 +1030

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,23 +9,24 @@
 #!/bin/sh
 import sys
 import math
+import os
 
 
 LIST_OF_APPS="python-smbus python-matplotlib python-pip gfortran libi2c-dev python-setuptools libblas-dev libatlas-base-dev liblapack-dev python-dev mysql-server python-mysqldb phpmyadmin"
 
-print('cd')
-print('sudo apt-get update -y')
-print('sudo apt-get install -y $LIST_OF_APPS')
+os.system('cd')
+os.system('sudo apt-get update -y')
+os.system('sudo apt-get install -y $LIST_OF_APPS')
 
-print('sudo apt-get update -y')
+os.system('sudo apt-get update -y')
 
-print('cd')
-print('sudo git clone https://github.com/topher2880/SDL_Pi_GroveWeatherPi')
-print('sudo git clone https://github.com/adafruit/Adafruit_Python_PureIO.git')
-print('cd Adafruit_Python_PureIO')
-print('sudo python setup.py install')
-print('cd')
+os.system('cd')
+os.system('sudo git clone https://github.com/topher2880/SDL_Pi_GroveWeatherPi')
+os.system('sudo git clone https://github.com/adafruit/Adafruit_Python_PureIO.git')
+os.system('cd Adafruit_Python_PureIO')
+os.system('sudo python setup.py install')
+os.system('cd')
 
-print('sudo easy_install scipy -y')
-print('sudo pip install tentacle_pi')
+os.system('sudo easy_install scipy -y')
+os.system('sudo pip install tentacle_pi')
 
