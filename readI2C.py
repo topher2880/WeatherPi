992e870e1ae56f9a14961443d764862bfc9dea01
Topher Webb <mining@ozecash.com>
2017-03-19 21:52:55 +1030

    Update readI2C.py

--- readI2C.py
+++ readI2C.py
@@ -20,26 +20,29 @@ while True:
  data = ""
  print "-----------"
  try:
-   print "block 1"
-   data = bus.read_i2c_block_data(address, 0);
-   print ' '.join(hex(x) for x in data)
- except IOError:
-   #subprocess.call(['i2cdetect', '-y', '1'])
-   flag = 1 #optional flag to signal your code to resend or something
- #time.sleep(3)
- try:
-   print "block 2"
-   data = bus.read_i2c_block_data(address, 1);
-   print ' '.join(hex(x) for x in data)
-   print "-----------"
- except IOError:
-   #subprocess.call(['i2cdetect', '-y', '1'])
-   flag = 1 #optional flag to signal your code to resend or something
- print "flag=", flag
+  print "block 1"
+ data = bus.read_i2c_block_data(address, 0);
+ print ' '.join(hex(x) for x in data)
+except IOError:
+ #subprocess.call(['i2cdetect', '-y', '1'])
+ flag = 1 #optional flag to signal your code to resend or something
+#time.sleep(3)
+ try
+ print "block 2"
+ data = bus.read_i2c_block_data(address, 1);
+ print ' '.join(hex(x) for x in data)
+ print "-----------"
+except IOError:
+ #subprocess.call(['i2cdetect', '-y', '1'])
+ flag = 1 #optional flag to signal your code to resend or something
+ 
+print "flag=", flag
  if (flag == 0):
-   twoblockMessages = twoblockMessages + 1
+  twoblockMessages = twoblockMessages + 1
  else:
-   errorMessages = errorMessages + 1
-  print "twoblock = %i error = %i percent error=%6.2f%%" % (twoblockMessages, errorMessages, 100*(float(errorMessages)/float(errorMessages+twoblockMessages)))
+  errorMessages = errorMessages + 1
+ 
+ print "twoblock = %i error = %i percent error=%6.2f%%" % (twoblockMessages, errorMessages, 100*(float(errorMessages)/float(errorMessages+twoblockMessages)))
  time.sleep(2);
  
+  
